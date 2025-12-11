ba1ed7b79c87ee1bf6360f1e2307bdd27801ac9d
XenonAdonis <hrgaba@gmail.com>
2025-12-11 11:32:02 -0600

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
+// Import BrowserRouter instead of HashRouter
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'; 
 import Welcome from './pages/Welcome';
-import SpineSafety from './pages/SpineSafety';
-import PickupGuide from './pages/PickupGuide';
-import CommonMistakes from './pages/CommonMistakes';
-import HandlingTips from './pages/HandlingTips';
+// ... (omitted page imports)
 
 const App: React.FC = () => {
   return (
-    <HashRouter>
+    // Use BrowserRouter
+    <BrowserRouter> 
       <Routes>
         <Route path="/" element={<Welcome />} />
-        <Route path="/spine-safety" element={<SpineSafety />} />
-        <Route path="/pickup-guide" element={<PickupGuide />} />
-        <Route path="/common-mistakes" element={<CommonMistakes />} />
-        <Route path="/handling-tips" element={<HandlingTips />} />
+        {/* ... (rest of routes) ... */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
